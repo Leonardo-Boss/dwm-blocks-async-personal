f08e140d88039c7044f4b4091400e057c20e983d
Leonardo-Boss <70913810+Leonardo-Boss@users.noreply.github.com>
2023-08-16 13:18:19 -0300

    some other scripts

--- config.c
+++ config.c
@@ -4,16 +4,10 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"lescript.py",    600,    11},
+    {"pingopi",    60,    11},
+    {"sb-forecast",18000,  5},
+    {"sb-date",    60,    10},
 };
 
 const unsigned short blockCount = LEN(blocks);
