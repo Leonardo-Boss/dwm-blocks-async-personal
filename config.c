cecb50ca0691c585232d051fd8d2274fed17d2bd
Leonardo Boss <leonardoboss2001@gmail.com>
2024-10-18 20:37:17 -0300

    stuff

--- config.c
+++ config.c
@@ -4,8 +4,9 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"lescript.py",600,   11},
-    {"sb-xscreensaver",0,   6},
+    {"tomldo", 1, 9},
+    {"sb-mic", 0,   20},
+    {"sb-internet", 600, 6},
     {"pingopi",    60,    12},
     {"sb-forecast",900,  5},
     {"sb-date",    60,    10},
